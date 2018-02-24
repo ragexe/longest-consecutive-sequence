12282784ca1af02cc24328ad992c037277535a8c
ragexe <ragexe@mail.ru>
2018-02-24 11:56:26 +0300

    longest-consecutive-sequence/feat: done but not o(n), is it possible without sort?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let answer = 0;
+    let temp = 1;
+
+    if (array.length < 2) {
+        return array.length
+    }
+
+    array = array.sort((a, b) => a - b);
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] - 1 === array[i - 1] || array[i] === array[i - 1]) {
+            temp += array[i] - array[i - 1];
+        } else {
+            temp = 1;
+        }
+        if (temp > answer) answer = temp
+    }
+
+    return answer
+};
